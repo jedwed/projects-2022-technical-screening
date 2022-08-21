25a172ad6237a0b978c479470854e01594889984
jedwed <83894732+jedwed@users.noreply.github.com>
2022-08-21 15:39:41 +1000

    Passed tests

--- handbook.py
+++ handbook.py
@@ -47,20 +47,96 @@ def manually_fix_conditions():
         CONDITIONS["COMP4952"] = 'COMP4952'
     if "COMP" not in CONDITIONS["COMP4953"]:
         CONDITIONS["COMP4953"] = 'COMP4953'
-    CONDITIONS["COMP9491"] = re.sub("units oc credit", "units of credit", CONDITIONS["COMP9491"])
     
 def parse_conditions(condition_string):
     # Replace multiple spaces with single space
     condition_string = re.sub(r' +', r' ', condition_string)
     # Remove redundant 'prerequisite' text at beginning
     condition_string = re.sub(r'^[Pp]re[A-Za-z-]*:*\s?', r'', condition_string)
-    return condition_string
-
-    # TODO: UOC checking
-    uoc_regex = r'([0-9]{1,3}) units o[fc] credit (?:in)? (level \d \w{4} courses|([^)]+))'
+    # Remove redundant 'completion of': this implies UOC condition
+    condition_string = re.sub(r'[Cc]ompletion of ', r'', condition_string)
+    # Separating brackets with space so they tokenize
+    condition_string = re.sub(r'\(', r'( ', condition_string)
+    condition_string = re.sub(r'\)', r' )', condition_string)
+    # UOC checking
+    condition_string = re.sub(r',', r'', condition_string)
+    condition_string = re.sub(r'level (\d) (\w{4}) courses', r'L\1_\2', condition_string)
+    condition_string = re.sub(r'(\w{4}) courses', r'\1', condition_string)
+    condition_string = re.sub(r'([0-9]{1,3}) units o[fc] credit in ([^\s])', r'\1_UOC_in \2', condition_string)
+    condition_string = re.sub(r'([0-9]{1,3}) units o[fc] credit', r'\1_UOC', condition_string)
+    condition_string = re.sub(r'([0-9]{1,3}) units o[fc] credit in \((.*?)\)', r'\1_UOC_in \2', condition_string)
+    return condition_string.split()
 
 def validate_conditions(courses_list, conditions):
-    if re.fullmatch(r'\w{4}\d{4}', conditions):
-        return conditions in courses_list
+    if conditions == []:
+        return True
 
-print(is_unlocked(["COMP3411", "COMP1511"], "COMP4418"))
+    next_idx = 1
+    if conditions[0] == '(':
+        to_idx = find_closing_bracket(conditions)
+        next_idx = to_idx + 1
+        current_condition = validate_conditions(courses_list, conditions[1:to_idx])
+    elif "UOC" in conditions[0]:
+        # If UOC is in the course code gg
+        uoc_tokens = conditions[0].split('_')
+        uoc_required = int(uoc_tokens[0])
+        if len(uoc_tokens) == 3:
+            if conditions[1] == '(':
+                to_idx = find_closing_bracket(conditions[1:]) + 1
+                next_idx = to_idx + 1
+                current_condition = validate_uoc_req_list(courses_list, uoc_required, conditions[2:to_idx])
+            else:   
+                current_condition = validate_uoc_req(courses_list, uoc_required, conditions[1])
+                next_idx = 2
+        else:
+            current_condition = len(courses_list) * 6 >= uoc_required
+    else:
+        current_condition = conditions[0] in courses_list
+
+    # Base case: if there are no more conditions to evaluate
+    if len(conditions) == next_idx:
+        return current_condition
+
+    if re.fullmatch(r'and', conditions[next_idx], flags=re.I):
+        return current_condition and validate_conditions(courses_list, conditions[next_idx + 1:])
+    elif re.fullmatch(r'or', conditions[next_idx], flags=re.I):
+        return current_condition or validate_conditions(courses_list, conditions[next_idx + 1:])
+
+    # If we reach this point it's doomed gg
+    return False
+
+def find_closing_bracket(conditions):
+    stack = []
+    unclosed_counter = 0
+    for i, token in enumerate(conditions):
+        if token == '(':
+            unclosed_counter += 1
+        if token == ')':
+            unclosed_counter -= 1
+        if unclosed_counter == 0:
+            return i
+
+    # This return statement is solely for decorative purposes
+    return -1
+
+def validate_uoc_req(courses_list, uoc_required, courses_required):
+    total_uoc_met = 0
+    if re.fullmatch(r'\w{4}', courses_required):
+        for course in courses_list:
+            if re.search(rf'^{courses_required}', course):
+                total_uoc_met += 6
+    elif re.search(r'L(\d)_(\w{4})', courses_required):
+        # Determind wtf the courses string is actually saying
+        level, course_code = re.search(r'L(\d)_(\w{4})', courses_required).groups()
+        for course in courses_list:
+            if re.search(rf'^{course_code}{level}', course):
+                total_uoc_met += 6
+    return total_uoc_met >= uoc_required
+
+def validate_uoc_req_list(courses_list, uoc_required, courses_required):
+    total_uoc_met = 0
+    for course in courses_list:
+        if course in courses_required:
+            total_uoc_met += 6
+    return total_uoc_met >= uoc_required
+
